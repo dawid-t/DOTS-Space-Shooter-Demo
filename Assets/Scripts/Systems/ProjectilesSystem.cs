b08f7f96ee09bd783424ebeef758199a4a126279
dawid-t <dawidt@protonmail.com>
2020-11-24 23:44:43 +0100

    Set a better collision precision with projectiles

--- Assets/Scripts/Systems/ProjectilesSystem.cs
+++ Assets/Scripts/Systems/ProjectilesSystem.cs
@@ -21,22 +21,42 @@ public class ProjectilesSystem : ComponentSystem
 				return;
 			}
 
-			// Destroy if collided with asteroid and update the score:
+			// Destroy if collided with asteroid and update the score (use more quadrant cells for better collision precision):
 			int hashMapKey = QuadrantSystem.GetQuadrantHashMapKey(translation.Value);
-			QuadrantSystem.QuadrantEntityData quadrantData;
-			if(AsteroidsSystem.IsCollisionWithAsteroid(hashMapKey, entity, entity.Index, translation, QuadrantSystem.QuadrantAsteroidsMultiHashMap, out quadrantData))
-			{
-				if(entity != Entity.Null)
-				{
-					PostUpdateCommands.DestroyEntity(entity);
-				}
-				if(quadrantData.entity != Entity.Null)
-				{
-					PostUpdateCommands.DestroyEntity(quadrantData.entity);
-				}
+			QuadrantSystem.QuadrantEntityData[] quadrantData = new QuadrantSystem.QuadrantEntityData[9];
 
-				ShipSystem.Score++;
-				UI.UpdateScore(ShipSystem.Score);
+			bool[] isCollision = new bool[9];
+			isCollision[0] = AsteroidsSystem.IsCollisionWithAsteroid(hashMapKey, entity, entity.Index, translation, QuadrantSystem.QuadrantAsteroidsMultiHashMap, out quadrantData[0]);
+
+			isCollision[1] = AsteroidsSystem.IsCollisionWithAsteroid(hashMapKey+1, entity, entity.Index, translation, QuadrantSystem.QuadrantAsteroidsMultiHashMap, out quadrantData[1]);
+			isCollision[2] = AsteroidsSystem.IsCollisionWithAsteroid(hashMapKey-1, entity, entity.Index, translation, QuadrantSystem.QuadrantAsteroidsMultiHashMap, out quadrantData[2]);
+
+			isCollision[3] = AsteroidsSystem.IsCollisionWithAsteroid(hashMapKey+QuadrantSystem.QuadrantYMultiplier, entity, entity.Index, translation, QuadrantSystem.QuadrantAsteroidsMultiHashMap, out quadrantData[3]);
+			isCollision[4] = AsteroidsSystem.IsCollisionWithAsteroid(hashMapKey-QuadrantSystem.QuadrantYMultiplier, entity, entity.Index, translation, QuadrantSystem.QuadrantAsteroidsMultiHashMap, out quadrantData[4]);
+
+			isCollision[5] = AsteroidsSystem.IsCollisionWithAsteroid(hashMapKey+1+QuadrantSystem.QuadrantYMultiplier, entity, entity.Index, translation, QuadrantSystem.QuadrantAsteroidsMultiHashMap, out quadrantData[5]);
+			isCollision[6] = AsteroidsSystem.IsCollisionWithAsteroid(hashMapKey-1+QuadrantSystem.QuadrantYMultiplier, entity, entity.Index, translation, QuadrantSystem.QuadrantAsteroidsMultiHashMap, out quadrantData[6]);
+
+			isCollision[7] = AsteroidsSystem.IsCollisionWithAsteroid(hashMapKey+1-QuadrantSystem.QuadrantYMultiplier, entity, entity.Index, translation, QuadrantSystem.QuadrantAsteroidsMultiHashMap, out quadrantData[7]);
+			isCollision[8] = AsteroidsSystem.IsCollisionWithAsteroid(hashMapKey-1-QuadrantSystem.QuadrantYMultiplier, entity, entity.Index, translation, QuadrantSystem.QuadrantAsteroidsMultiHashMap, out quadrantData[8]);
+
+			for(int i = 0; i < isCollision.Length; i++)
+			{
+				if(isCollision[i])
+				{
+					if(entity != Entity.Null)
+					{
+						PostUpdateCommands.DestroyEntity(entity);
+					}
+					if(quadrantData[i].entity != Entity.Null)
+					{
+						PostUpdateCommands.DestroyEntity(quadrantData[i].entity);
+					}
+
+					ShipSystem.Score++;
+					UI.UpdateScore(ShipSystem.Score);
+					break;
+				}
 			}
 		});
 	}

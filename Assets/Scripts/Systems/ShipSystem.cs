f444b0f829237b0fee60a509be7f8cc262ccc30f
dawid-t <dawidt@protonmail.com>
2020-11-24 06:34:54 +0100

    Add a camera mechanic that follows the player

--- Assets/Scripts/Systems/ShipSystem.cs
+++ Assets/Scripts/Systems/ShipSystem.cs
@@ -7,6 +7,8 @@ public class ShipSystem : ComponentSystem
 {
 	protected override void OnUpdate()
 	{
+		Camera mainCamera = Camera.main;
+
 		Entities.ForEach((Entity entity, ref Translation translation, ref Rotation rotation, ref ShipData shipData) =>
 		{
 			if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
@@ -35,6 +37,8 @@ public class ShipSystem : ComponentSystem
 			
 			float3 forwardVector = math.mul(rotation.Value, new float3(0, -1, 0));
 			translation.Value += forwardVector * shipData.Speed * shipData.SpeedMultiplier * Time.deltaTime;
+
+			mainCamera.transform.position = new Vector3(translation.Value.x, translation.Value.y, mainCamera.transform.position.z);
 		});
 	}
 }
